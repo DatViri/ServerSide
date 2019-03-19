7508e28ad226312c2e6d187c51606df62c6e0dfa
Dat Truong <dat.truonghuu1212@gmail.com>
2019-03-19 22:12:08 +0200

    add file upload

--- week_1/index.js
+++ week_1/index.js
@@ -1,13 +1,37 @@
 const express = require('express');
 const multer = require('multer');
-
+const bodyParser = require('body-parser');
+const path = require('path');
 
 const app = express();
 
-app.use(express.static('public'));
+app.use(express.static('./public'));
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
 
-app.get('/', (req, res) => {
-  res.render('index');
+const storage = multer.diskStorage({
+  destination: './public/uploads/',
+  filename: (req, file, cb)=> {
+    cb(null, file.fieldname + '-' + Date.now() +
+    path.extname(file.originalname));
+  },
 });
 
-app.listen(3000);
+const upload = multer({
+  storage: storage,
+}).single('myImage');
+
+app.post('/upload', (req, res) => {
+  upload(req, res, (err) => {
+    if (err) {
+      console.log('wrong');
+    } else {
+      const body = req.body;
+      console.log(body);
+    }
+  });
+});
+
+
+app.listen(3000, ()=> console.log('Listening on port 3000'));
+
