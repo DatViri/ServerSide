6fbedd1229e62aa2f637ab58a4aa837c38b8bf92
Dat Truong <dat.truonghuu1212@gmail.com>
2019-03-21 17:54:28 +0200

    refactor code

--- week_1/index.js
+++ week_1/index.js
@@ -1,61 +1,23 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const mongoClient = require('mongodb');
-const multer = require('multer');
-const fs = require('fs');
-const ObjectId = require('mongodb').ObjectId;
-const url = 'mongodb+srv://dattruong:7121997@image-view-q4pfx.mongodb.net/test?retryWrites=true';
+const router = require('./router');
+const mongoose = require('mongoose');
 
-
+const url = 'mongodb+srv://dattruong:7121997@image-view-q4pfx.mongodb.net/datDB?retryWrites=true';
 const app = express();
-const upload = multer({limits: {fileSize: 1000000}, dest: 'public/uploads/'});
+
+// Middleware
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
-app.post('/api/upload', upload.single('picture'), (req, res, next)=>{
-  if (req.file == null) {
-    res.render('Please select a picture file to submit!');
-  } else {
-    mongoClient.connect(url, (err, client)=>{
-      const newImg = fs.readFileSync(req.file.path);
-      const encImg = newImg.toString('base64');
-      const newItem = {
-        category: req.body.category,
-        title: req.body.title,
-        description: req.body.description,
-        contentType: req.file.mimeType,
-        size: req.file.size,
-        img: new Buffer(encImg, 'base64'),
-      };
-      const db = client.db('image-view-info');
-      db.collection('info').insertOne(newItem, (err, result)=>{
-        if (err) {
-          console.log(err);
-        }
-        console.log('saved to database');
-        res.redirect('/api');
-      });
-    });
-  }
+// Connect DB
+mongoose.connect(url).then(()=>{
+  console.log('Connected successfully.');
+  app.listen(3000);
+}, (err) => {
+  console.log('Connection to db failed: ' + err);
 });
 
-app.get('/api/:picture', (req, res)=>{
-  const filename = req.params.picture;
-  mongoClient.connect(url, (err, client)=>{
-    db = client.db('image-view-info');
-    db.collection('info').findOne({'_id': ObjectId(filename)}, (err, result)=>{
-      res.contentType('image/jpeg');
-      res.send(result.img.buffer);
-      console.log(result.category);
-    });
-  });
-});
+// API
+app.use('/api', router);
 
-
-app.listen(3000, () => {
-  console.log('Running on port 3000');
-});
-
-app.get('/api', (req, res) => {
-  res.sendfile(__dirname + '/public/index.html');
-});
